98f90baee17c10ea73087c4c56430c031dfc615d
Will Moore <w.moore@dundee.ac.uk>
2015-06-10 22:55:30 +0100

    Started adding to ShapeEditor.js - installed 'watch'

--- src/js/docs.js
+++ src/js/docs.js
@@ -1,5 +1,16 @@
+/* globals ShapeEditor: false */
+/* globals console: false */
+
 $(function() {
 
     var shapeEd = new ShapeEditor("shapesCanvas", 512, 512);
 
+
+    // set state depending on what we want to do,
+    // for example to create Rectangle
+    shapeEd.setState("RECT");
+
+    shapeEd.setState("invalid");
+
+    console.log("getState", shapeEd.getState());
 });
